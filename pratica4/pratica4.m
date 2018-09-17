65a24acf8092c30342c96341d622c68215f5eba4
DANIEL VENTURINI <a1724487@debian9.dacom>
2018-09-17 16:41:22 -0300

    Projeto 4 finalizado

--- pratica4/pratica4.m
+++ pratica4/pratica4.m
@@ -3,7 +3,7 @@ pkg load image
 # (1,2) carrengando a imagem e convertendo para double
 imagemEntrada = im2double(imread('pratica4.jpg'));
 
-# (3) monte um filtro de média com tamanho 9x9
+# (3) monte um filtro de mï¿½dia com tamanho 9x9
 filtro = zeros(9, 9, 'double');
 
 for i=1:9
@@ -12,26 +12,40 @@ for i=1:9
    endfor
 endfor
 
-# (4) Use a função filter2(b,x, shape) como forma para aplicar o filtro de média do passo 3
-#     na imagem no formato double obtida no passo 2. Essa função irá fazer a correlação do
-#     filtro sobre a imagem. Nesse passo use o valor padrão para o parâmetro shape. 
+# (4) Use a funï¿½ï¿½o filter2(b,x, shape) como forma para aplicar o filtro de mï¿½dia do passo 3
+#     na imagem no formato double obtida no passo 2. Essa funï¿½ï¿½o irï¿½ fazer a correlaï¿½ï¿½o do
+#     filtro sobre a imagem. Nesse passo use o valor padrï¿½o para o parï¿½metro shape. 
 imagemSaida = im2double(filter2(filtro, imagemEntrada, 'same'));
 
 # (5) Exiba a imagem resultante do passo 4 e observe a borda da imagem. O que aconteceu?
-# (R) Aplicamos o meio do filtro em cada pixel da borda. Depois é multiplicado o filtro por 0,
+# (R) Aplicamos o meio do filtro em cada pixel da borda. Depois ï¿½ multiplicado o filtro por 0,
 #     e a borda fica preta.
 imshow(imagemSaida);
 
-# (6) Vamos aplicar o filtro da média novamente só que agora vamos replicar os pixels
-#     da borda da imagem do passo 2 antes com a função padarray(...).
-#     Forneça os parâmetros para essa função apropriadamente considerando o tamanho
-#     do filtro de média.
+# (6) Vamos aplicar o filtro da mï¿½dia novamente sï¿½ que agora vamos replicar os pixels
+#     da borda da imagem do passo 2 antes com a funï¿½ï¿½o padarray(...).
+#     Forneï¿½a os parï¿½metros para essa funï¿½ï¿½o apropriadamente considerando o tamanho
+#     do filtro de mï¿½dia.
 
 imagemSaida = padarray(imagemEntrada, [4 4], 'replicate');
 
-# (7) Aplique novamente o mesmo filtro da média, só que agora sobre a imagem com
+# (7) Aplique novamente o mesmo filtro da mï¿½dia, sï¿½ que agora sobre a imagem com
 #  padding do passo 6, e exiba a imagem resultante
 imagemSaida = filter2(filtro, imagemEntrada, 'valid');
 #figure, imshow(imagemSaida);
 
-# (8) Monte os filtros de Sobel (disponíveis nos slides).
+# (8) Monte os filtros de Sobel (disponï¿½veis nos slides).
+filtroSobel1 = [-1, -2, -1; 0, 0, 0; 1, 2, 1];
+filtroSobel2 = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
+
+# (9) Aplicamos o padding e depois o filtro Sobel
+imagemSobelResultado = padarray(imagemSobelResultado, [1 1], 'replicate');
+imagemSobel1 = filter2(filtroSobel1, imagemEntrada, 'valid');
+imagemSobel2 = filter2(filtroSobel2, imagemEntrada, 'valid');
+
+imagemSobelResultado = zeros(306, 306, 'double');
+
+# (10) Magnitude do gradiente
+imagemSobelResultado = abs(imagemSobel1) + abs(imagemSobel2);
+
+figure, imshow(imagemSobelResultado);
