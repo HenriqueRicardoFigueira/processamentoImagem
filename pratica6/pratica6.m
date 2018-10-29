406d1c83003c957b1512cc4643b3b1e691ad9643
danielventurini <danielventurini021@gmail.com>
2018-10-29 17:12:02 -0300

    Exercicios de 1 a 6 prontos

--- pratica6/pratica6.m
+++ pratica6/pratica6.m
@@ -1,4 +1,30 @@
-pkg load image;
+pkg load image;                         # 1
 
-imgEntrada = imread(pratica6.png);
-imshow(imgEntrada);
+function res = filtroPassaBaixa(imagem, P, Q)
+  for x=1:P
+    for y=1:Q
+      imagem = imagem
+    end
+  end
+
+  res = imagem
+endfunction
+
+
+
+imgEntrada = imread('pratica6.png');    # 2
+imgEntrada = im2double(imgEntrada);     # 3
+# tamanho da imagem
+M = 256;
+N = 256;
+
+P = 2*M;
+Q = 2*N;
+
+imgPadding = fft2(imgEntrada, P, Q);    # 4
+imgPadding = fftshift(imgPadding);      # 5
+imgPadding = uint8(abs(imgPadding));    # 6
+
+filtro = filtroPassaBaixa(imgPadding, P, Q);
+
+figure, imshow(filtro);
