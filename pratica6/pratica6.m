3292a73d039bb07c359a1a97520a60c2ae817c69
Daniel Venturini <danielventurini021@gmail.com>
2018-10-30 10:32:52 -0300

    Prática 6 quase pronta

--- pratica6/pratica6.m
+++ pratica6/pratica6.m
@@ -1,12 +1,12 @@
 pkg load image;                         # 1
 
 function bra = distanciaEuclidiana(u, v, p, q)
-  bra = (((u - p/2) ^ 2) + ((v - q/2) ^ 2))*0.5;
+  bra = (((u - p/2) ^ 2) + ((v - q/2) ^ 2)) ^ (1/2);
 endfunction
 
 function res = filtroPassaBaixa(imagem, P, Q)       # 7
   D0 = 20;
-  h = zeros(P, Q, 'uint8');
+  h = zeros(P, Q);
 
   for u=1:P
     for v=1:Q
@@ -25,11 +25,23 @@ imgEntrada = im2double(imgEntrada);     # 3
 P = 2*M;
 Q = 2*N;
 
-imgPadding = fft2(imgEntrada, P, Q);    # 4
-imgPadding = fftshift(imgPadding);      # 5
-imgEspectro = uint8(abs(imgPadding));   # 6
+imgTransformada = fft2(imgEntrada, P, Q);     # 4
+imgTransformada = fftshift(imgTransformada);  # 5
+figure, imshow(uint8(abs(imgTransformada)));  # 6
 
-filtro = filtroPassaBaixa(imgPadding, P, Q);
-imgFiltro = imgEspectro .* filtro;      # 8
+filtro = filtroPassaBaixa(imgTransformada, P, Q);
+imgFiltro = imgTransformada .* filtro;        # 8
+figure, imshow(real(imgFiltro));
+imgTransformada = ifftshift(imgTransformada); # 9
+imgTransformada = ifft2(imgTransformada);     # 10
+imgTransformada = real(imgTransformada);      # 11
 
-figure, imshow(imgFiltro);
+# 12
+imgSaida = zeros(M, N);
+for x=1:M
+  for y=1:N
+    imgSaida(x, y) = imgTransformada(x, y);
+  end
+end
+
+figure, imshow(im2uint8(imgSaida));
