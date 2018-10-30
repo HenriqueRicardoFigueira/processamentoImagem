781c7e9b41b0d1b5bbda64359360306092c568be
Daniel Venturini <danielventurini021@gmail.com>
2018-10-30 09:31:37 -0300

    Até o passdo 8 pronto

--- pratica6/pratica6.m
+++ pratica6/pratica6.m
@@ -1,10 +1,21 @@
 pkg load image;                         # 1
 
-function res = filtroPassaBaixa(imagem, P, Q)
-  res = imagem;
+function bra = distanciaEuclidiana(u, v, p, q)
+  bra = (((u - p/2) ^ 2) + ((v - q/2) ^ 2))*0.5;
 endfunction
 
+function res = filtroPassaBaixa(imagem, P, Q)       # 7
+  D0 = 20;
+  h = zeros(P, Q, 'uint8');
 
+  for u=1:P
+    for v=1:Q
+      Duv = distanciaEuclidiana(u, v, P, Q);
+      h(u, v) = e ^ ((-Duv ^ 2) / (2 * (D0 ^ 2)));
+    end
+  end
+  res = h;
+endfunction
 
 imgEntrada = imread('pratica6.png');    # 2
 imgEntrada = im2double(imgEntrada);     # 3
@@ -16,8 +27,9 @@ Q = 2*N;
 
 imgPadding = fft2(imgEntrada, P, Q);    # 4
 imgPadding = fftshift(imgPadding);      # 5
-imgPadding = uint8(abs(imgPadding));    # 6
+imgEspectro = uint8(abs(imgPadding));   # 6
 
 filtro = filtroPassaBaixa(imgPadding, P, Q);
+imgFiltro = imgEspectro .* filtro;      # 8
 
-figure, imshow(filtro);
+figure, imshow(imgFiltro);
